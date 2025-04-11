f5b538601ba71002f82423f7dbff517a084f4da2
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-04-11 16:03:06 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
   images:{
     remotePatterns:[
       {
@@ -10,6 +19,9 @@ const nextConfig: NextConfig = {
         port: '',
       }
     ]
+  },
+  typescript:{
+    ignoreBuildErrors: true
   }
 };
 
