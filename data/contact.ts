e3a3d0bc342acb6b7cb55eec9aa28ed135720946
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-06-16 18:44:48 +0530

    contact page  data

---
+++ data/contact.ts
@@ -0,0 +1,23 @@
+
+ export const contactInfo = [
+    {
+
+      image: '/mail.png',
+      title: "Email",
+      value: "hello@desyn.com",
+      description: "Drop us a line anytime"
+    },
+    {
+
+      image:'/office-building.png',
+      title: "Office",
+      value: "San Francisco, CA",
+      description: "Come visit our HQ"
+    },
+    {
+      image:'/quick-response.png',
+      title: "Response Time",
+      value: "< 24 hours",
+      description: "We're pretty quick"
+    }
+  ];
