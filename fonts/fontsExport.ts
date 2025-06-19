f7fc6d4bf3fc3ca351db680dac6b14bb76aaf4b5
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-06-19 21:19:12 +0530

    fonts export

---
+++ fonts/fontsExport.ts
@@ -0,0 +1,9 @@
+import localFont from "next/font/local";
+
+export const moogalatorFont = localFont({
+    src: "./Moogalator-yYJr3.ttf",
+}); 
+
+export const superChargeFont = localFont({
+    src: "./SuperCharge.otf"
+})
