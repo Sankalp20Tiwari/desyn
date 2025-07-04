be8af12bebaec100da7b4c4d8941a8268d0997b5
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-07-04 17:57:13 +0530

    font removed

--- fonts/fontsExport.ts
+++ fonts/fontsExport.ts
@@ -1,8 +1,5 @@
 import localFont from "next/font/local";
 
-export const moogalatorFont = localFont({
-    src: "./Moogalator-yYJr3.ttf",
-}); 
 
 export const superChargeFont = localFont({
     src: "./SuperCharge.otf"
